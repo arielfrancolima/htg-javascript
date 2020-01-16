1843f7a9a2b11a48752a191a194e7d030272c169
arielfrancolima <duarteluis2014@hotmail.com>
2020-01-16 16:10:23 -0300

    realize los ejercicios 1, 2, 3

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,28 @@
-console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+/*
+let favorito = document.querySelector('.comment');
+console.log(favorito.classList.add);// Es recomendable usar el console para averiguar que parte del codigo falla
+favorito.classList.add('comentFavorito');
+
+//Creamos una variable, donde se guarda el elemento que selecionamos (querySelector(''))
+//luego añadimos la clase donde se guardo todos los estilos. 
+
+
+let elementoHr = document.querySelectorAll("hr");
+for (let i=0 ; i<elementoHr.length; i++){
+    elementoHr[i].classList.add('comentFavorito');
+}
+*/
+
+
+const comentarioLargo = document.querySelectorAll('.comment--text');
+
+for(let i=0; i < comentarioLargo.length; i++){
+    if(comentarioLargo[i].innerText.length > 50){
+        comentarioLargo[i].classList.add('ocultar');
+    }
+}
+
+
+
+
+
