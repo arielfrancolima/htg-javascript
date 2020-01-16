b109c3204f1e85dbcf6a29570c901dfb5d50c82e
arielfrancolima <duarteluis2014@hotmail.com>
2020-01-16 17:34:48 -0300

    Finalize el trabajo de la parte tecnica, faltaria la parte no tecnica

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,4 +1,4 @@
-/*
+
 let favorito = document.querySelector('.comment');
 console.log(favorito.classList.add);// Es recomendable usar el console para averiguar que parte del codigo falla
 favorito.classList.add('comentFavorito');
@@ -11,7 +11,7 @@ let elementoHr = document.querySelectorAll("hr");
 for (let i=0 ; i<elementoHr.length; i++){
     elementoHr[i].classList.add('comentFavorito');
 }
-*/
+
 
 
 const comentarioLargo = document.querySelectorAll('.comment--text');
@@ -21,6 +21,26 @@ for(let i=0; i < comentarioLargo.length; i++){
         comentarioLargo[i].classList.add('ocultar');
     }
 }
+
+
+
+let comentarioMasLargo = document.querySelectorAll('.comment--text');
+console.log(comentarioMasLargo);
+
+let comentMax = comentarioMasLargo[0];
+for(let i = 1; i<comentarioMasLargo.length; i++){
+
+    if (comentarioMasLargo[i].innerText.length > comentMax.innerText.length){
+        comentMax = comentarioMasLargo[i];
+        }
+    }
+    console.dir(comentMax.classList,add);
+    comentMax.classList.add('ocultar');
+
+    // Metodo contiene una funcion
+    //Propiedad de un objeto puede tener un valor de tipo string, number, etc
+
+
 
 
 
